48e5af4196f21a96e0cfb852a75dd54712f981b6
Thiago Henrique <thiagohp070@gmail.com>
2025-06-23 17:14:38 -0300

    init project

---
+++ src/test/java/com/iconit/tech_be/TechBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iconit.tech_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
