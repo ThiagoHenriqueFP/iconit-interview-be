ffc9b6c704b7bfefa2e7b6a9ade84c210f3b1980
Thiago Henrique <thiagohp070@gmail.com>
2025-06-25 20:22:15 -0300

    feat: update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,9 @@
-insert into products (id, code, description, product_type, supplier_price, stock_quantity, is_available) values (1, 'asd1', 'random product', 0, 30.1, 100, true);
-insert into products (id, code, description, product_type, supplier_price, stock_quantity, is_available) values (2, 'asd2', 'random product', 0, 42, 3, true);
-insert into products (id, code, description, product_type, supplier_price, stock_quantity, is_available) values (3, 'asd3', 'random product', 1, 44, 20, true);
-insert into products (id, code, description, product_type, supplier_price, stock_quantity, is_available) values (4, 'asd4', 'random product', 2, 405, 20, true);
+insert into products (code, description, product_type, supplier_price, stock_quantity, is_available) values ( 'axp-123', 'Smartphone', 0, 30.1, 100, true);
+insert into products (code, description, product_type, supplier_price, stock_quantity, is_available) values ( 'asdf', 'GPU Nvidia', 0, 42, 3, true);
+insert into products (code, description, product_type, supplier_price, stock_quantity, is_available) values ( 'abcd33', 'Wash machine', 1, 44, 20, true);
+insert into products (code, description, product_type, supplier_price, stock_quantity, is_available) values ( 'asert-x03', 'Couch', 2, 405, 20, true);
+
+insert into stock_history (product_id, movement, sell_value, sell_date, sell_quantity, total_value, is_canceled) values (1, 0, 40.5, '2025-06-25 14:21:54.617341', 10, 405, false);
+insert into stock_history (product_id, movement, sell_value, sell_date, sell_quantity, total_value, is_canceled) values (1, 1, 40.5, '2025-06-25 14:21:54.617341', 100, 4050, false);
+insert into stock_history (product_id, movement, sell_value, sell_date, sell_quantity, total_value, is_canceled) values (2, 0, 40.5, '2025-06-25 14:21:54.617341', 10, 405, false);
+insert into stock_history (product_id, movement, sell_value, sell_date, sell_quantity, total_value, is_canceled) values (3, 0, 40.5, '2025-06-25 14:21:54.617341', 10, 405, false);
